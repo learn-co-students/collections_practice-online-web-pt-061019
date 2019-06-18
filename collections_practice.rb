2b02865cd1f8a39db1eede67f1bf91ed4fcd38a2
poignant-firmware-1783 <elevatemedfit@gmail.com>
2019-06-18 18:18:02 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,41 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort do |a,b|
+    b<=>a
+  end
+end
+
+def sort_array_char_count(array)
+  array.sort_by {|obj| obj.size}
+end
+def swap_elements(array)
+  array[1],array[2]=array[2],array[1]
+  array
+end
+def reverse_array(array)
+  array.reverse
+end
+def kesha_maker(array)
+  array.each do |name|
+    name[2]= "$"
+  end
+end
+def find_a(array)
+  array.find_all {|pet| pet.start_with?("a")}
+end
+def sum_array(array)
+  array.sum
+end
+def add_s(array)
+  array.each do |word|
+    nuarray =[]
+    if word != array[1]
+      nuword = word << "s"
+      nuarray << nuword
+    end
+    nuarray
+  end
+end
